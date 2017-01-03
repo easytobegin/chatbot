a40e75da456410e24f2c511cdf735d12ac8ec9c9
minisheep <minisheep@minisheepdeMacBook-Pro.local>
2017-01-03 11:58:05 +0800

    新增cityMap城市中英文映射

---
+++ src/com/minisheep/Bean/CityMap.java
@@ -0,0 +1,117 @@
+package com.minisheep.Bean;
+
+public class CityMap {
+	private int airportId;
+	private String iataCode;
+	private String icaoCode;
+	private String airportCNName; 
+	private String airportENName;
+	private String airportNature;
+	private String cnabbr1W;
+	private String cnabbr2W;
+	private String cityId;
+	private String airportTimeZone;
+	private String displayCNName;
+	private String displayENName;
+	private String countryCode;
+	private String logguid;
+	private String lastUpdated;
+	private String firstPinYin;
+	
+	public int getAirportId() {
+		return airportId;
+	}
+	public void setAirportId(int airportId) {
+		this.airportId = airportId;
+	}
+	public String getIataCode() {
+		return iataCode;
+	}
+	public void setIataCode(String iataCode) {
+		this.iataCode = iataCode;
+	}
+	public String getIcaoCode() {
+		return icaoCode;
+	}
+	public void setIcaoCode(String icaoCode) {
+		this.icaoCode = icaoCode;
+	}
+	public String getAirportCNName() {
+		return airportCNName;
+	}
+	public void setAirportCNName(String airportCNName) {
+		this.airportCNName = airportCNName;
+	}
+	public String getAirportENName() {
+		return airportENName;
+	}
+	public void setAirportENName(String airportENName) {
+		this.airportENName = airportENName;
+	}
+	public String getAirportNature() {
+		return airportNature;
+	}
+	public void setAirportNature(String airportNature) {
+		this.airportNature = airportNature;
+	}
+	public String getCnabbr1W() {
+		return cnabbr1W;
+	}
+	public void setCnabbr1W(String cnabbr1w) {
+		cnabbr1W = cnabbr1w;
+	}
+	public String getCnabbr2W() {
+		return cnabbr2W;
+	}
+	public void setCnabbr2W(String cnabbr2w) {
+		cnabbr2W = cnabbr2w;
+	}
+	public String getCityId() {
+		return cityId;
+	}
+	public void setCityId(String cityId) {
+		this.cityId = cityId;
+	}
+	public String getAirportTimeZone() {
+		return airportTimeZone;
+	}
+	public void setAirportTimeZone(String airportTimeZone) {
+		this.airportTimeZone = airportTimeZone;
+	}
+	public String getDisplayCNName() {
+		return displayCNName;
+	}
+	public void setDisplayCNName(String displayCNName) {
+		this.displayCNName = displayCNName;
+	}
+	public String getDisplayENName() {
+		return displayENName;
+	}
+	public void setDisplayENName(String displayENName) {
+		this.displayENName = displayENName;
+	}
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	public String getLogguid() {
+		return logguid;
+	}
+	public void setLogguid(String logguid) {
+		this.logguid = logguid;
+	}
+	public String getLastUpdated() {
+		return lastUpdated;
+	}
+	public void setLastUpdated(String lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+	public String getFirstPinYin() {
+		return firstPinYin;
+	}
+	public void setFirstPinYin(String firstPinYin) {
+		this.firstPinYin = firstPinYin;
+	}
+}
